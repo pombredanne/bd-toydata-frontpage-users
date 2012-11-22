2de1f0a2e42360583c25133701263b8ac7525e92
Ville Tuulos <ville@bitdeli.com>
2012-11-22 02:16:35 +0000

    add charts

--- __main__.py
+++ __main__.py
@@ -1,9 +1,38 @@
 import bitdeli
-from bitdeli.widgets import Map, set_theme
+from bitdeli.widgets import Users, Text, set_theme, get_themes
 
-set_theme('eighties')
+print '\n'.join(get_themes())
+
+set_theme('lime')
+
+HASHES = ['205e460b479e2e5b48aec07710c08d50',
+          '25c7c18223fb42a4c6ae1c8db6f50f9b',
+          'x',
+          'f0a7bfdc9c509e8ea02b3ad25ba768b6',
+          'b42c651650ec8d3d95829c75e318af2d',
+          'd0ae789ef12347de8ec30b3765dd5d89',
+          'x',
+          '476d8afffbfa06fa852e677848c2388d']
 
 for profile in bitdeli.profiles():
     pass
 
-Map(size=(12, 7), data={'IN': 30})
+Text(size=(12, 2),
+     head='All systems UP!')
+Text(label='Conversions',
+     size=(4, 2),
+     head='8.13%',
+     color=3)
+Text(label='Revenue',
+     size=(4, 2),
+     head='$6,412',
+     color=2)
+Text(label='Avg. Latency',
+     size=(4, 2),
+     head='320ms')
+Text(size=(3, 2),
+     head='8 new users:',
+     color=1)
+Users(size=(9, 2),
+      large=True,
+      data=[{'gravatar_hash': h} for h in HASHES])
